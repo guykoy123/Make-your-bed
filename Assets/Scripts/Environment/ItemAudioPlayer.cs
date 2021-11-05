c9f75d0da2193eb38b0605719d15b5831172f120
Triceracop <23092757+guykoy123@users.noreply.github.com>
2021-11-05 18:27:50 +0200

    added paper sound, reworked collision sound

--- Assets/Scripts/Environment/ItemAudioPlayer.cs
+++ Assets/Scripts/Environment/ItemAudioPlayer.cs
@@ -5,33 +5,24 @@ using UnityEngine;
 public class ItemAudioPlayer : MonoBehaviour
 {
     AudioSource audioSource;
-    float PreviousVelocity;
-    float Velocity = 0;
-    Rigidbody rb;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
         audioSource.playOnAwake = false;
         audioSource.spatialBlend = 1f;
-        audioSource.volume = OptionsData.masterVolume;
-        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        PreviousVelocity = Velocity;
-        Velocity = rb.velocity.magnitude;
     }
 
-    public void UpdateVolume()
-    {
-        //audioSource.volume = OptionsData.masterVolume;
-    }
     public void OnCollisionEnter(Collision collision)
     {
-        audioSource.volume = Mathf.InverseLerp(0, 1, PreviousVelocity);
         audioSource.Play();
     }
 }
